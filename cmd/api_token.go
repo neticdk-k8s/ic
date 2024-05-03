1f8074272aa282290c28e6b33340b2fb86f8b5eb
Kim Nørgaard <kn@netic.dk>
2024-05-03 09:45:56 +0200

    feat: add api-token command to get an access token that can be used in scripts (#3)

---
+++ cmd/api_token.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// New creates a new api-token command
+func NewAPITokenCmd(ec *ExecutionContext) *cobra.Command {
+	o := apiTokenOptions{}
+	c := &cobra.Command{
+		Use:     "api-token",
+		Short:   "Get access token for the API",
+		Long:    "Get access token for the API. It uses the cached token if it is valid and performs login otherwise.",
+		GroupID: groupAuth,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return o.run(ec)
+		},
+	}
+	return c
+}
+
+type apiTokenOptions struct{}
+
+func (o *apiTokenOptions) run(ec *ExecutionContext) error {
+	logger := ec.Logger.WithPrefix("Login")
+	ec.Authenticator.SetLogger(logger)
+
+	tokenSet, err := doLogin(ec)
+	if err != nil {
+		return fmt.Errorf("logging in: %w", err)
+	}
+
+	fmt.Println(tokenSet.AccessToken)
+	return nil
+}

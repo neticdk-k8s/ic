1c9175e0e7125f70022ff217ba21f9faa3ef4b5d
Kim Nørgaard <kn@netic.dk>
2025-03-14 10:47:49 +0100

    fix: formatting

--- internal/tokencache/cache.go
+++ internal/tokencache/cache.go
@@ -6,10 +6,10 @@ import (
 
 // Cache represents a token caching interface
 type Cache interface {
-    // Lookup retrieves a token set from the cache
+	// Lookup retrieves a token set from the cache
 	Lookup(key Key) (*oidc.TokenSet, error)
-    // Save stores a token set in the cache
+	// Save stores a token set in the cache
 	Save(key Key, tokenSet oidc.TokenSet) error
-    // Invalidate removes a token set from the cache
+	// Invalidate removes a token set from the cache
 	Invalidate(key Key) error
 }
